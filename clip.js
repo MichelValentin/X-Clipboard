c9676c6e01e650b182c19baab6aa546591f58a04
Michel Valentin <michel.valentin@gmail.com>
2018-11-08 10:10:03 +0100

    Add files via upload

--- clip.js
+++ clip.js
@@ -7,7 +7,7 @@ function atou(str) {
 }
 
 function OnLoadFn() {
-    $('#text').html("");
+    document.getElementById("text").innerHTML = "";
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = OnReadyStateChangeFn;
     xmlhttp.open("GET", "clip.php", true);
@@ -15,9 +15,9 @@ function OnLoadFn() {
 }
 
 function OnClickFn() {
-    var xmlhttp = new XMLHttpRequest();
+	var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = OnReadyStateChangeFn;
-    var text = $('#text').text();
+    var text = document.getElementById("text").value;
     text = utoa(text); 
     xmlhttp.open("POST", "clip.php", true);
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -25,36 +25,37 @@ function OnClickFn() {
 }
 
 function CopyFn() {
-    $('#text').select();
+    var copyText = document.getElementById("text");
+    copyText.select();
     document.execCommand("Copy");
 }
 
 function OnReadyStateChangeFn() {
     if (this.readyState == 4) {
         $('#urltxt').html("");  
-        if (this.status == 200) {
+      	if (this.status == 200) {
             var myObj = JSON.parse(this.responseText);
             var text = atou(myObj.text); 
-            $('#text').html(text);
+            document.getElementById("text").innerHTML = text;
             var re = /(\w+\:\/\/(?:[\w]+\.)?[\w]+\.(?:\S+))/ig;
             var result = "";
             var urllist = "";
             while((result = re.exec(text)) != null) {
                 urllist = urllist + '<a href="' + result[0] + '" class="list-group-item" target="_blank">' + result[0] + '</a>';
-            }
+            	}
             if (urllist.length > 0) {
-                $('#urltxt').html(urllist);
+            	$('#urltxt').html(urllist);
+            	}
             }
-        }
-        else {
-            var message = this.status + ' ' + this.statusText;
-            var html = '<div>';
-            html += '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>';
-            html += '<strong>Error</strong> ' + message;
-            html += '</div>';        
-            $('#req_error').html(html);
-            $('#req_error').show();
-        }
+            else {
+            	var message = this.status + ' ' + this.statusText;
+            	var html = '<div>';
+            	html += '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>';
+            	html += '<strong>Error</strong> ' + message;
+            	html += '</div>';        
+            	$('#req_error').html(html);
+            	$('#req_error').show();
+            }
     }
 }
 
