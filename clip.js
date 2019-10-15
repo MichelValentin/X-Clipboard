96158e9e5fc1a8d3604ee000a1c36817acea0445
Michel Valentin <michel.valentin@gmail.com>
2019-10-15 16:15:23 +0200

    Update clip.js

--- clip.js
+++ clip.js
@@ -1,9 +1,9 @@
 function utoa(str) {
-    return window.btoa(encodeURIComponent(str));
+  return window.btoa(unescape(encodeURIComponent( str )));
 }
 
 function atou(str) {
-    return decodeURIComponent(window.atob(str));
+  return decodeURIComponent(escape(window.atob( str )));
 }
 
 function OnLoadFn() {
