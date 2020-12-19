069054bf2a9cda376b52229920952fd4164e933e
Michel <michel.valentin@gmail.com>
2020-12-19 16:27:12 +0100

    eslint

--- clip.js
+++ clip.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 function utoa(str) {
     return window.btoa(encodeURIComponent(str));
 }
@@ -15,13 +16,13 @@ function OnLoadFn() {
 }
 
 function OnClickFn() {
-	var xmlhttp = new XMLHttpRequest();
+    var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = OnReadyStateChangeFn;
     var text = document.getElementById("text").value;
-    text = utoa(text); 
+    text = utoa(text);
     xmlhttp.open("POST", "clip.php", true);
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.send("text=" + text);     
+    xmlhttp.send("text=" + text);
 }
 
 function CopyFn() {
@@ -32,29 +33,30 @@ function CopyFn() {
 
 function OnReadyStateChangeFn() {
     if (this.readyState == 4) {
-        $('#urltxt').html("");  
-      	if (this.status == 200) {
+        $('#urltxt').html("");
+        if (this.status == 200) {
             var myObj = JSON.parse(this.responseText);
-            var text = atou(myObj.text); 
+            var text = atou(myObj.text);
             document.getElementById("text").innerHTML = text;
-            var re = /(\w+\:\/\/(?:[\w]+\.)?[\w]+\.(?:\S+))/ig;
+            var re = /(\w+:\/\/(?:[\w]+\.)?[\w]+\.(?:\S+))/ig;
             var result = "";
             var urllist = "";
-            while((result = re.exec(text)) != null) {
+            // eslint-disable-next-line no-cond-assign
+            while ((result = re.exec(text)) != null) {
                 urllist = urllist + '<a href="' + result[0] + '" class="list-group-item" target="_blank">' + result[0] + '</a>';
             }
             if (urllist.length > 0) {
-            	$('#urltxt').html(urllist);
+                $('#urltxt').html(urllist);
             }
         }
         else {
-           	var message = this.status + ' ' + this.statusText;
-           	var html = '<div>';
-           	html += '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>';
-           	html += '<strong>Error</strong> ' + message;
-           	html += '</div>';        
-           	$('#req_error').html(html);
-           	$('#req_error').show();
+            var message = this.status + ' ' + this.statusText;
+            var html = '<div>';
+            html += '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>';
+            html += '<strong>Error</strong> ' + message;
+            html += '</div>';
+            $('#req_error').html(html);
+            $('#req_error').show();
         }
     }
 }
